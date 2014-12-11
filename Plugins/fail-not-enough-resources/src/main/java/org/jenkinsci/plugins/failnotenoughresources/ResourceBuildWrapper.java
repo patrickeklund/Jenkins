e21b986970a0cbf27af5224d12c7e489edff29b1
Patrick Eklund <patrickeklund@hotmail.com>
2014-12-11 14:37:44 +0100

    new items.

--- Plugins/fail-not-enough-resources/src/main/java/org/jenkinsci/plugins/failnotenoughresources/ResourceBuildWrapper.java
+++ Plugins/fail-not-enough-resources/src/main/java/org/jenkinsci/plugins/failnotenoughresources/ResourceBuildWrapper.java
@@ -29,60 +29,126 @@ import java.io.IOException;
  *
  * <p>
  * before a build is performed, the {@link #setUp(AbstractBuild, Launcher, BuildListener)}
- * method will be invoked. 
+ * method will be invoked.
  *
  * @author Patrick Eklund
  */
 public class ResourceBuildWrapper extends BuildWrapper {
 
+    /**
+     * @param configDiskLimitVariable
+     *   parameter for disk limit.
+     */
     private String configDiskLimitVariable;
-    
+
+    /**
+     * Function that gets the class name.
+     * 
+     * @return String
+     *   Returns the String representation of the Class name.
+     **/
     @SuppressWarnings("unused")
 	private String getLongName() {
         return this.getClass().getName();
     }
 
+    /**
+     * Function that gets the class 'simple' name.
+     * 
+     * @return String
+     *   Returns the String representation of the Class 'Simple' name.
+     **/
     private String getSimpleName() {
-        return this.getClass().getSimpleName() ;
+        return this.getClass().getSimpleName();
     }
 
+    /**
+     * Function that gets the Display name.
+     * 
+     * @return String
+     *   Returns the Message.properties id for ResourceBuildWrapper.DisplayName.
+     **/
+    @SuppressWarnings("unused")
     public String getName() {
-        return getSimpleName() ;
+    	return Messages.ResourceBuildWrapper_DisplayName();
     }
-    
 
-    
-    public static boolean isInteger(String s) {
+    /**
+     * Function that checks if a string value is an integer.
+     * 
+     * @param value
+     *   String parameter that is used to to check if its an integer.
+     *   
+     * @return boolean
+     *   Returns whether the String was an integer or not.
+     **/
+    public static boolean isInteger(String value) {
         try { 
-            Integer.parseInt(s); 
+            Integer.parseInt(value); 
         } catch(NumberFormatException e) { 
             return false; 
         }
         // only got here if we didn't return false
         return true;
     }
-    
 
-    public void println(BuildListener listener, String Line1, String Line2) {
-		listener.getLogger().println( String.format("%20s: %25s : [%s]", getSimpleName(), Line1 , Line2 ));
-    }
-    public void println(BuildListener listener, String Line1, String Line2, String Line3) {
-		listener.getLogger().println( String.format("%20s: %25s : [%s] %s", getSimpleName(), Line1 , Line2, Line3));
-    }
-    public void println(BuildListener listener, String Line1, int Int1, String Line2) {
-		listener.getLogger().println( String.format("%20s: %25s : [%d] %s", getSimpleName(), Line1 , Int1, Line2 ));
-    }
-    
     /**
-     * This method returns the value of the configuration item configTextBox.
+     * Function that prints a formated message to the console log.
+     * 
+     * @param listener
+     *   Listener that we can use to send output to.
+     * @param Line1
+     *   First Line to print.
+     * @param Line2
+     *   Second Line to print.
+     **/
+    public void println(BuildListener listener, String Line1, String Line2) {
+		listener.getLogger().println(String.format("%20s: %25s : [%s]", getSimpleName(), Line1, Line2));
+    }
+
+    /**
+     * Function that prints a formated message to the console log.
+     * 
+     * @param listener
+     *   Listener that we can use to send output to.
+     * @param Line1
+     *   First Line to print.
+     * @param Line2
+     *   Second Line to print.
+     * @param Line3
+     *   Third Line to print.
+     **/
+    public void println(BuildListener listener, String Line1, String Line2, String Line3) {
+		listener.getLogger().println(String.format("%20s: %25s : [%s] %s", getSimpleName(), Line1, Line2, Line3));
+    }
+
+    /**
+     * Function that prints a formated message to the console log.
+     * 
+     * @param listener
+     *   Listener that we can use to send output to.
+     * @param Line1
+     *   First Line to print.
+     * @param int
+     *   Firts integer to print.
+     * @param Line2
+     *   Second Line to print.
+    **/
+    public void println(BuildListener listener, String Line1, int Int1, String Line2) {
+		listener.getLogger().println(String.format("%20s: %25s : [%d] %s", getSimpleName(), Line1, Int1, Line2));
+    }
+
+    /**
+     * This method returns the value of the configuration item configDiskLimit.
      *
      * The method name is bit awkward because <tt>config.jelly</tt> calls this method by the naming convention.
+     * 
+     * @return String
+     *   Returns the string value for configDiskLimit.
      **/
     public String getconfigDiskLimit() {
         return this.configDiskLimitVariable;
     }
-
-    
 
     /**
      *  Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
@@ -96,34 +162,36 @@ public class ResourceBuildWrapper extends BuildWrapper {
     	 **/
     }
 
- 
     /**
-     * This method sets the value of the configuration item configTextBox.
+     * This method sets the value of the configuration item configDiskLimit.
+     * Empty value will result in global value being used instead
      *
      * The method name is bit awkward because <tt>config.jelly</tt> calls this method by the naming convention.
+     *
+     * @param String
+	 *   String value to set the job configDiskLimit
+	 * 
+	 * @throws AbortException
+	 *   Trying to save a none numerical value will result in an AbortException being thrown
      **/
     @DataBoundSetter 
     public void setconfigDiskLimit(String configDiskLimit) throws AbortException { 
-    	if ( configDiskLimit.isEmpty() ) 
+    	if (configDiskLimit.isEmpty() || isInteger(configDiskLimit)) { 
     		this.configDiskLimitVariable = configDiskLimit;
-    	else if ( isInteger(configDiskLimit) )
-    		this.configDiskLimitVariable = configDiskLimit;
-    	else
-    		throw new AbortException( "Please remove or set Integer value [" + configDiskLimit + "] Isn't a valid valuer");	
+    	} else {
+    		throw new AbortException("Please remove or set Integer value [" + configDiskLimit + "] Isn't a valid valuer");
+    	}
     } 
 
-    
     @SuppressWarnings("rawtypes")
 	@Override
-    public Environment setUp( AbstractBuild build, Launcher launcher, BuildListener listener) {
+    public Environment setUp(AbstractBuild build, Launcher launcher, BuildListener listener) {
 
     	println(listener, "Inside", "setUp");
     	return new Environment() {
 		}; 
     }
-    
-    
-    
+
     /**
 	 * Overridden preCheckout.
 	 * 
@@ -135,11 +203,11 @@ public class ResourceBuildWrapper extends BuildWrapper {
 	public void preCheckout(AbstractBuild build, Launcher launcher,	BuildListener listener) throws IOException, InterruptedException, AbortException {
 
 		// Assign local variables
-		String BuiltOnStr  = build.getBuiltOnStr();
-		if (BuiltOnStr == "") {
-			BuiltOnStr = "Master";
+		String BuiltOnStr = build.getBuiltOnStr();
+		if (BuiltOnStr.equals("")) {
+           BuiltOnStr = "Master";
 		}
-		int ConvertedNodeDiskSpace = (int) ( DiskSpaceMonitor.DESCRIPTOR.get(build.getBuiltOn().toComputer()).size / (1024 * 1024 * 1024));
+		int ConvertedNodeDiskSpace = (int) (DiskSpaceMonitor.DESCRIPTOR.get(build.getBuiltOn().toComputer()).size/(1024 * 1024 * 1024));
 		int DiskLimitUsed = -1;
 		int globalDiskLimitVariable = getDescriptor().getglobalDiskLimit();
 
@@ -149,11 +217,11 @@ public class ResourceBuildWrapper extends BuildWrapper {
 		 * 
 		 * Job configuration value is forcefully set to "" or integer value
 		 */
-		if ( ! configDiskLimitVariable.isEmpty() )
-			DiskLimitUsed = Integer.parseInt(configDiskLimitVariable);
-		else
-			DiskLimitUsed = globalDiskLimitVariable;
-
+		if (!configDiskLimitVariable.isEmpty()) {
+            DiskLimitUsed = Integer.parseInt(configDiskLimitVariable);
+		} else {
+            DiskLimitUsed = globalDiskLimitVariable;
+		}
 		println(listener, "Inside", "preCheckout");
 		println(listener, "Built On", BuiltOnStr);
 		println(listener, "Space Left on Node", ConvertedNodeDiskSpace , "Gb" );
@@ -161,14 +229,16 @@ public class ResourceBuildWrapper extends BuildWrapper {
 		println(listener, "Job Threshold", configDiskLimitVariable, "Gb");
 		println(listener, "Using Threshold", DiskLimitUsed, "Gb" );
 		
-		if ( ConvertedNodeDiskSpace < DiskLimitUsed )
-			throw new AbortException( "Aborting Build Diskspace[" + ConvertedNodeDiskSpace + "] < Threshold [" + DiskLimitUsed +"]");
+		if (ConvertedNodeDiskSpace < DiskLimitUsed) {
+            throw new AbortException("Aborting Build Diskspace[" + ConvertedNodeDiskSpace + "] < Threshold [" + DiskLimitUsed +"]");
+        }
 	}    
 
-
-    // Overridden for better type safety.
-    // If your plugin doesn't really define any property on Descriptor,
-    // you don't have to do this.
+    /*
+     *  Overridden for better type safety.
+     *  If your plugin doesn't really define any property on Descriptor,
+     *  you don't have to do this.
+     */
     @Override
     public DescriptorImpl getDescriptor() {
         return (DescriptorImpl)super.getDescriptor();
@@ -181,8 +251,7 @@ public class ResourceBuildWrapper extends BuildWrapper {
      */
 	@Extension // This indicates to Jenkins that this is an implementation of an extension point.
 	public static class DescriptorImpl extends BuildWrapperDescriptor { 
-    	
-   
+
         /**
          * To persist global configuration information,
          * simply store it in a field and call save().
@@ -229,19 +298,24 @@ public class ResourceBuildWrapper extends BuildWrapper {
         	//load the global parameters.
         	super.load();
         	
-        	if ( value.isEmpty() )
-       			return FormValidation.warning("Unless set, the global value [" + getglobalDiskLimit() + "] will be used");
-        	else if ( ! isInteger(value) )
-                return FormValidation.error("Please: Only use digits");
+//    		return FormValidation.warning("Unless set, the global value [" + getglobalDiskLimit() + "] will be used");
+        	if (value.isEmpty()) {
+        		return FormValidation.warning(Messages.ResourceBuildWrapper_GlobalValueUsed(getglobalDiskLimit()));
+        	} else if (!isInteger(value)) {
+                return FormValidation.error(Messages.ResourceBuildWrapper_OnlyUseDigits());
+        	}
             return FormValidation.ok();
         }
+
         public FormValidation doCheckGlobalDiskLimit(@QueryParameter String value) throws IOException, ServletException {
 			// This is how you include a value for a form check
-        	if ( value.isEmpty() )
-                return FormValidation.error("Please: Enter digit value");
-        	if ( ! isInteger(value))
-                return FormValidation.error("Please: Only enter digits");
-            return FormValidation.ok();
+        	if (value.isEmpty()) {
+                return FormValidation.error(Messages.ResourceBuildWrapper_EnterDigits());
+        	}
+        	if (!isInteger(value)) {
+                return FormValidation.error(Messages.ResourceBuildWrapper_OnlyUseDigits());
+        	}
+        	return FormValidation.ok();
         }
 
         @SuppressWarnings("rawtypes")
@@ -254,9 +328,15 @@ public class ResourceBuildWrapper extends BuildWrapper {
          * This human readable name is used in the configuration screen.
          **/
         public String getDisplayName() {
-            return ResourceBuildWrapper.class.getSimpleName().toString() + " : " + ResourceBuildWrapper.class.getName().toString();
+            return Messages.ResourceBuildWrapper_DisplayName();
         }
 
+        /**
+         * This method is used to store the global configuration items
+         *
+         * @return boolean
+         *      returns whether saving was successfull or not
+         **/
         @Override
         public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
             // To persist global configuration information,
@@ -268,17 +348,22 @@ public class ResourceBuildWrapper extends BuildWrapper {
             return super.configure(req,formData);
         }
 
-
-       
         /**
-         * This method returns the value of the configuration item globalTextBox.
+         * This method returns the value of the global configuration item globalDiskLimit.
          *
          * The method name is bit awkward because <tt>global.jelly</tt> calls this method by the naming convention.
+         * 
+         * @return int
+         *      returns the Global Disk limit
+         *      Always reverts to default, value of 1 Gb, if no value is set
          **/
         public int getglobalDiskLimit() {
-            return this.globalDiskLimitVariable;
+            if (globalDiskLimitVariable == 0) {
+        	    return 1;
+            } else {
+                return this.globalDiskLimitVariable;
+            }
         }
-
     }
 }
 
