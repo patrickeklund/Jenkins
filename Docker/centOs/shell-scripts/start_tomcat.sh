a093661964ddd6e3921428db3e2da7214937c014
Patrick Eklund <patrickeklund@hotmail.com>
2015-01-17 15:58:43 +0100

    proper bin folder

--- Docker/centOs/shell-scripts/start_tomcat.sh
+++ Docker/centOs/shell-scripts/start_tomcat.sh
@@ -3,5 +3,5 @@
 #
 # start tomcat
 #
-/opt/apache-tomcat-${TOMCAT_VERSION}-instance-1/scripts/start.sh
-/opt/apache-tomcat-${TOMCAT_VERSION}-instance-2/scripts/start.sh
+/opt/apache-tomcat-${TOMCAT_VERSION}-instance-1/bin/startup.sh
+/opt/apache-tomcat-${TOMCAT_VERSION}-instance-2/bin/startup.sh
