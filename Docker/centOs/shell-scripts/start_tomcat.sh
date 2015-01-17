cce27c4fa25ddc81feca14fb3af64dce6a26f951
Patrick Eklund <patrickeklund@hotmail.com>
2015-01-17 15:29:31 +0100

    added multiple apache instances to centos

---
+++ Docker/centOs/shell-scripts/start_tomcat.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+#
+# start tomcat
+#
+/opt/apache-tomcat-${TOMCAT_VERSION}-instance-1/scripts/start.sh
+/opt/apache-tomcat-${TOMCAT_VERSION}-instance-2/scripts/start.sh
--- Docker/docker_CreateAndStart.sh
+++ Docker/docker_CreateAndStart.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
 # Create the images
-docker build -t="patrick/eklund:jenkins_1.594" base/
-docker build -t="patrick/eklund:custom_jenkins_1.594" custom/
+#docker build -t="patrick/eklund:jenkins_1.594" base/
+#docker build -t="patrick/eklund:custom_jenkins_1.594" custom/
+docker build -t="local:apache" centOs/
 
 # Run the application in daemon mode
-docker run -d -p 8080:8080 --name Jenkins patrick/eklund:custom_jenkins_1.594
+#docker run -d -p 8080:8080 --name Jenkins patrick/eklund:custom_jenkins_1.594
+#docker run -d -p 8080:8080 --name Jenkins patrick/eklund:custom_jenkins_1.594
+docker run -i -p 8181:8181 -p 8282:8282 --name centos local:apache /bin/bash
 
 # list processes
 docker ps -a
