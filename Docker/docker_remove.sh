239269b271f487d744a9c2be5b1ee819a3bfc1fb
Patrick Eklund <patrickeklund@hotmail.com>
2015-01-25 14:43:42 +0100

    new

--- Docker/docker_remove.sh
+++ Docker/docker_remove.sh
@@ -16,27 +16,26 @@ usage () {
 
     echo
     echo "Usage:"
-    echo "  -all     :  stop all docker images"
-    echo "  -jbase   :  stop Jenkins Base docker image"
-    echo "  -jCustom :  stop Jenkins Custmom docker image"
-    echo "  -centos  :  stop centos docker image"
+    echo "  -all     :  remove all docker images"
+    echo "  -jbase   :  remove Jenkins Base docker image"
+    echo "  -jCustom :  remove Jenkins Custmom docker image"
+    echo "  -centos  :  remove centos docker image"
     exit
 }
 
 
 remove_dockerImage() {
 
-    if [ $(docker ps -a | grep $1 | awk {'print $1'}) ] ; then
-        echo Remove any [$1] running container forcefully, stopping it when needed
-        docker rm -f $(docker ps -a | grep $1 | awk {'print $1'})
+    if [ $(docker ps -a | grep $1:$2 | awk {'print $1'}) ] ; then
+        echo Remove any [$1:$2] running container forcefully, stopping it when needed
+        docker rm -f $(docker ps -a | grep $1:$2 | awk {'print $1'})
     fi
     
-
-    if [ $(docker images --filter 'image=$1') ] ; then
-       echo Delete all all images related to [$1]
-       docker rmi $(docker images --filter 'image=$1')
+    if [ $(docker images -a | grep $1 | grep $2 | awk {'print $3'} ) ] ; then
+       echo Remove all all images related to [$1:$2]
+       docker rmi $(docker images -a | grep $1 | grep $2 | awk {'print $3'})
     else
-       echo No images to delete related to [$1]
+       echo No images present related to [$1:$2]
     fi
 }
 
@@ -48,11 +47,12 @@ remove_allDockerImage() {
        docker rm -f $(docker ps -a -q)    
     fi
 
-    if [ ”$(docker images -q)” == ”null” ] ; then
-       echo Delete all all images
+    var=$(docker images -q)
+    if [ ”$var” != ”” ] ; then
+       echo Remove all all images
        docker rmi $(docker images -q)
     else
-       echo No images to delete
+       echo No images present
     fi
 }
 
@@ -66,11 +66,11 @@ case "$1" in
 
 -all)       remove_allDockerImage
             ;;
--jbase)     remove_dockerImage    local/jenkins:1.594
+-jbase)     remove_dockerImage    local/jenkins   1.594
             ;;
--jcustom)   remove_dockerImage    local/jenkins:1.594_custom
+-jcustom)   remove_dockerImage    local/jenkins   1.594_custom
             ;;
--centos)    remove_dockerImage    local/apache:tomcat
+-centos)    remove_dockerImage    local/apache    tomcat
             ;;
 *)          usage $#
             ;;
---
+++ Docker/docker_start.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# Run the application in daemon mode
+#docker run -d -p 8080:8080 --name Jenkins patrick/eklund:custom_jenkins_1.594
+#docker run -d -p 8080:8080 --name Jenkins patrick/eklund:custom_jenkins_1.594
+#docker run -i -p 8181:8181 -p 8282:8282 --name centos -t local:apache /bin/bash
+
